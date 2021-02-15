9ca9824349d0867689dad8e33be2c07c4093eb45
mishanko <m.mikhaylov@krypton.moscow>
2021-02-15 17:24:34 +0300

    just simple preliminary stuff

---
+++ hello.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+//func main() {
+//	fmt.Printf("I like Go!\n")
+//	fmt.Printf("I like Go!\n")
+//	fmt.Printf("I like Go!")
+//}
+//
+
+//func main(){
+//	fmt.Println("Hello Go"[0])
+//	fmt.Printf(string("Hello go"[0]))
+//}
+//func main(){
+//	var hello string
+//
+//	hello = "Hello Mike"
+//
+//	var a int = 2018
+//	fmt.Println(hello)
+//	fmt.Println(a)
+//}
+
+//func main(){
+//	var (
+//		name string = "Dima"
+//		age int = 15
+//	)
+//	fmt.Println(name)
+//	fmt.Println(age)
+//}
+//func main(){
+//	a := 5
+//	b := 10
+//	c := a + b
+//	fmt.Println(c)
+//}
+//func main(){
+//	var (
+//		name string
+//		age int
+//	)
+//	fmt.Print("Введите имя: ")
+//	fmt.Scan(&name) // ссылка на переменную
+//	fmt.Print("Введите возраст: ")
+//	fmt.Scan(&age)
+//	fmt.Println(name, age)
+//}
+func main() {
+	var d int = 100
+	d = 50
+
+	fmt.Println(d)
+}
